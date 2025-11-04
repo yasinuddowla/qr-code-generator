2cbf07b48d7b4eca2bd00981e3a4e041a317cf2e
yasinuddowla <yasin.uddowla@gmail.com>
2025-11-04 10:30:43 +0600

    Update Next.js configuration to enable strict mode, set output to 'export', enforce trailing slashes, and configure unoptimized images for static export.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
